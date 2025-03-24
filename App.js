5a8e0af01617ae551d358e06c6c1c9056374f393
jamuna1404 <jamunaarumugam1404@gmail.com>
2025-03-24 23:24:50 +0530

    Update App.js added forgot password

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./Forgot_password";
 import StaffLogin from "./stafflogin";
 import AdminLogin from "./adminlogin";
 
@@ -7,12 +8,15 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<StaffLogin />} />  {/* Default Route: Staff Login */}
-        <Route path="/admin-login" element={<AdminLogin />} /> {/* Admin Login Page */}
+        <Route path="/" element={<StaffLogin />} />  
+        <Route path="/admin-login" element={<AdminLogin />} /> 
+        <Route path="/Forgot_password" element={<ForgotPassword />} />
+
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
